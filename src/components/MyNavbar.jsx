5dfede48b2d5d5364eb72aaafcd05ec9b1e7610a
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-11-06 11:05:01 +0100

    sistemazione navbar

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -2,31 +2,59 @@ import { Button, Container, Form, Nav, Navbar, NavDropdown } from "react-bootstr
 
 const MyNavbar =() => {
     return (
-        <Navbar  bg="primary" data-bs-theme="dark">
+        <Navbar  bg="primary" data-bs-theme="light">
           <Container fluid>
             <Navbar.Brand href="#">Ricette Con Salvo</Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
+            <Navbar.Collapse id="navbarScroll" >
               <Nav
                 className="me-auto my-2 my-lg-0"
                 style={{ maxHeight: '100px' }}
                 navbarScroll
               >
                 <Nav.Link href="#action1">Home</Nav.Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
-                <NavDropdown title="Link" id="navbarScrollingDropdown">
-                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown title="Cottura">
+                  <NavDropdown.Item href="/" >
+                   al forno 
+                  </NavDropdown.Item>
                   <NavDropdown.Item href="#action4">
-                    Another action
+                   alla griglia
                   </NavDropdown.Item>
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="#action5">
-                    Something else here
+                    bolliti
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action5">
+                    fritto
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action5">
+                    alla brace
                   </NavDropdown.Item>
                 </NavDropdown>
-                <Nav.Link href="#" disabled>
-                  Link
-                </Nav.Link>
+                <NavDropdown title="Portata">
+                  <NavDropdown.Item href="#action3" >
+                  Antipasto 
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                  Primo
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                  Secondo
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action5">
+                  Dessert
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <NavDropdown title="Bevande" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">
+                  Alcolici 
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action3">
+                  Analcolici 
+                </NavDropdown.Item>
+                
+                </NavDropdown>
               </Nav>
               <Form className="d-flex">
                 <Form.Control
