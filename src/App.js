87d8675b1e2accec3d7d4ee5fe5e4f1f7df9c231
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-11-06 10:24:19 +0100

    sistemazione di Aoo.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import MyNavbar from './components/MyNavbar';
 import RecipeOven from './components/RecipeOven';
+import SingleRecipe from './components/SingleRecipe';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <>
-      <MyNavbar/>
-      <RecipeOven/>
-    </>
-  );
+    <BrowserRouter>
+    <MyNavbar />
+    <Routes>
+      <Route path="/" element={<RecipeOven />} />  {/* Pagina principale */}
+      <Route path="/recipe/:idRecipe" element={<SingleRecipe />} /> {/* Pagina della ricetta */}
+    </Routes>
+  </BrowserRouter>
+);
+  
 }
 
 export default App;
