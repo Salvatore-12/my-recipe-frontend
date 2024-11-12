e7b3e088844c8ef771a4185f7993aec1b0c63b61
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-11-12 11:24:30 +0100

    cancellazione percorso ed spostati nella cartella cooking methods

--- src/components/cooking methods/RecipeBoilling.jsx
+++ src/components/cooking methods/RecipeBoilling.jsx
@@ -1,32 +1,35 @@
-import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getRecipeOven } from "../Redux/action";
+import { getRecipeBoiling } from "../../Redux/action";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const RecipeOven = () => {
+const RecipeBoiling= () => {
     const dispatch = useDispatch();
-    const recipe = useSelector(state => state.recipeOven); 
+    const recipe = useSelector(state => state.recipeBoiling); 
 
     useEffect(() => {
-        dispatch(getRecipeOven());
+        dispatch(getRecipeBoiling());
     }, [dispatch]);
     console.log("Dati recipe:", recipe);
 
     if (!recipe) return <p>Caricamento in corso...</p>;
-    return(  <div>
+    return(  
+        <div className="d-flex">
         {recipe.map((recipe) => (
-            <div className="ms-3"  key={recipe.idRecipe}>
+            <div className="ms-3 customCard"  key={recipe.idRecipe}>
                 <Link  style={{
                                 textDecoration: 'none',
                                color: 'inherit', 
                                 }}
-                 className="text-dark" to={`/recipe/${recipe.idRecipe}`}>
+                 className="text-dark" 
+                 to={`/recipe/${recipe.idRecipe}`}>
                 <h1>{recipe.name || 'Nome non disponibile'}</h1>
                 <img 
                     src={recipe.imageUrl || 'immagine-default.jpg'} 
                     alt={recipe.name}  
-                    width="300" 
+                    className="recipe-image" 
                 />
+                <p className="MyDesciption">{recipe.description}</p>
             
                 </Link>
             </div>
@@ -38,4 +41,4 @@ const RecipeOven = () => {
     
 );
 }
-export default RecipeOven;
+export default RecipeBoiling
