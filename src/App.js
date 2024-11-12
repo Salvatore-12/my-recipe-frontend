133692b863265160836312b218e8c43c1b90a480
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-11-12 11:23:30 +0100

    sistemazione e aggiunta delle rotte

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import MyNavbar from './components/MyNavbar';
-import RecipeOven from './components/RecipeOven';
 import SingleRecipe from './components/SingleRecipe';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import RecipeBoiling from './components/RecipeBoilling';
+import RecipeBoiling from './components/cooking methods/RecipeBoilling';
+import FirstCourse from './components/dish category/FirstCourse';
+import RecipeOven from './components/cooking methods/RecipeOven';
 
 
 function App() {
@@ -13,7 +14,10 @@ function App() {
     <BrowserRouter>
     <MyNavbar />
     <Routes>
-      <Route path="/" element={<RecipeOven />} />
+      {/*route for routes for various dish category */}
+      <Route path = "/first-course" element = {<FirstCourse/>} />
+      {/*route for routes for various cooking methods */}
+      <Route path="/al-forno" element={<RecipeOven />} />
       <Route path="/Bollitura" element={<RecipeBoiling />} />
       <Route path="/recipe/:idRecipe" element={<SingleRecipe />} />
     </Routes>
