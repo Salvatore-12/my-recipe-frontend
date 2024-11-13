d058c43f0a779b337c68dd1a570f2bf5c23e63e8
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-11-13 16:24:20 +0100

    aggiunta icona del cestino per rimuore una ricetta dai preferiti

--- src/components/Favorities.jsx
+++ src/components/Favorities.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { setRemoveFavorite } from "../Redux/action";
+import { MdDelete } from "react-icons/md";
 
 
 const Favorites = () => {
@@ -19,32 +20,31 @@ const Favorites = () => {
     }
 
     return (
-        <Container fluid className="mt-4">
+        <Container fluid className="mt-4 ">
             <h2 className="mb-4">Ricette Preferite</h2>
             <Row>
                 {favorites.map((recipe) => (
                     <Col md={4} key={recipe.idRecipe} className="mb-4">
-                        <Card>
+                        <Card className="CardPrefer">
+                        <Link to={`/recipe/${recipe.idRecipe}`}>
                             <Card.Img
                                 variant="top"
                                 src={recipe.imageUrl || "immagine-default.jpg"}
                                 alt={recipe.name || "Nome non disponibile"}
                                 style={{ objectFit: "cover", height: "200px" }}
                             />
+                        </Link>
                             <Card.Body>
                                 <Card.Title>{recipe.name || "Nome non disponibile"}</Card.Title>
                                 <Card.Text>
                                     {recipe.description || "Descrizione non disponibile"}
                                 </Card.Text>
-                                <Link to={`/recipe/${recipe.idRecipe}`} className="btn btn-primary">
-                                    Visualizza Ricetta
-                                </Link>
+            
                                 <Button
-                                    variant="danger"
                                     onClick={() => handleRemoveFavorite(recipe.idRecipe)}
-                                    className="ml-2"
+                                    className="ml-2 IconRemoveFavorities"
                                 >
-                                    Rimuovi dai Preferiti
+                                    <MdDelete className="IconRemoveFavorities" />
                                 </Button>
                             </Card.Body>
                         </Card>
