980e079ff75b5ba4afd3e36975935da15829260e
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-11-19 12:42:31 +0100

    aggiunta la  route per la pagina dei risultati di ricerca

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import RecipeBoiling from './components/cooking methods/RecipeBoilling';
 import FirstCourse from './components/dish category/FirstCourse';
 import RecipeOven from './components/cooking methods/RecipeOven';
 import Favorites from './components/Favorities';
+import RecipeResults from './components/RecipeResults';
 
 
 function App() {
@@ -22,6 +23,8 @@ function App() {
       <Route path="/al-forno" element={<RecipeOven />} />
       <Route path="/Bollitura" element={<RecipeBoiling />} />
       <Route path="/recipe/:idRecipe" element={<SingleRecipe />} />
+       {/* route to search results */}
+       <Route path="/risultati-perNome" element={<RecipeResults />} />
     </Routes>
   </BrowserRouter>
 );
