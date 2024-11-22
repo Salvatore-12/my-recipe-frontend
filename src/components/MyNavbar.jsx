04df80348aee2f30697da1b8a2d6e836741ff3b7
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-11-22 19:08:43 +0100

    sistemazione link home

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -56,7 +56,7 @@ const MyNavbar =() => {
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
               <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-                  <Nav.Link as={Link} to="/Bollitura">Home</Nav.Link>
+                  <Nav.Link as={Link} to="/">Home</Nav.Link>
                   <NavDropdown title="Cottura">
                       <NavDropdown.Item as={Link} to="/al-forno">
                           al forno
