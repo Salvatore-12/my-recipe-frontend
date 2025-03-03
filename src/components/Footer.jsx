2269658833c26e156e05239e4c58b69babe84b61
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2025-03-03 11:23:31 +0100

    altre sistemazione sul componente e sulla classe css di riferimento al footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,21 +5,21 @@ const Footer = () => {
     return(
     <Container fluid className="FooterContainer">
     <Nav>
-    <div className="d-flex flex-row ms-4 ps-5 mt-4 mt-3">
-        <span className="d-flex flex-column ms-4 ps-4">
+    <div className="d-flex flex-row mt-4">
+        <span className="d-flex flex-column mr-4">
+        <h5>HELP</h5>
         <p>chi siamo</p>
         <p>Contattaci</p>
         <p>FAQ</p>
         </span>
         <span className="d-flex flex-column ms-4 ps-4 ">
             <h5>AREA LEGALE</h5>
-            <p>Termini e Condizioni di Acquisto</p>
             <p>Privacy Policy</p>
             <p>Cookie Policy</p>
         </span>
     </div>
 
-    <div className="icons">
+    <div className="ms-5 icons">
           <Telegram className="me-3" />
           <Tiktok className="me-3" />
           <Youtube className="me-3" />
