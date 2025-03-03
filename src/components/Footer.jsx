53d06acd01c177e0ecb3d9b467a19d9d50c96970
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2025-03-03 10:37:05 +0100

    creazione  componente Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import { Container, Nav } from "react-bootstrap"
+import { Facebook, Instagram,Telegram,Tiktok,Youtube } from "react-bootstrap-icons"
+
+const Footer = () => {
+    return(
+    <Container fluid className=" bg-success-subtle text-black pt-3 ms-0 me-0">
+    <Nav>
+    <div className="d-flex flex-row ms-4 ps-5 mt-4 mt-3">
+        <span className="d-flex flex-column ms-4 ps-4">
+        <p>chi siamo</p>
+        <p>Contattaci</p>
+        <p>FAQ</p>
+        </span>
+        <span className="d-flex flex-column ms-4 ps-4 ">
+            <h5>AREA LEGALE</h5>
+            <p>Termini e Condizioni di Acquisto</p>
+            <p>Privacy Policy</p>
+            <p>Cookie Policy</p>
+        </span>
+    </div>
+
+    <div className="icons">
+          <Telegram className="me-3" />
+          <Tiktok className="me-3" />
+          <Youtube className="me-3" />
+          <Instagram className="me-3" />
+          <Facebook className="me-3" />
+    </div>
+
+    </Nav>
+</Container>
+);    
+};
+export default Footer;
