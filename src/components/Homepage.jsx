15698879bb6d6f808389b480111a89a149fcca8a
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2025-05-05 10:36:49 +0200

    aggiungo carousel per le ricette estive sul componente homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,11 +4,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getAlcoholicDrink, getRecipeOven } from "../Redux/action";
 
+
 const Homepage = () => {
     const dispatch = useDispatch();
 
   const recipesOven = useSelector((state) => state.recipeOven);
   const recipesDrinks = useSelector((state) => state.alcoholicDrink);
+  const recipesSummer = useSelector((state) => state.recipeSummer);
 
   
   useEffect(() => {
@@ -61,6 +63,37 @@ const Homepage = () => {
         ) : (
           <Carousel className="CarouseCustom" interval={1500}>
             {recipesDrinks.map((recipe, index) => (
+              <Carousel.Item key={index}>
+                <img
+                  src={recipe.imageUrl || "immagine-default.jpg"}
+                  alt={recipe.name || "Immagine della ricetta"}
+                  className="d-block w-100 ImgCarouseCustom"
+                  style={{ height: "200px", objectFit: "cover" }}
+                />
+                <Carousel.Caption className="CarouselCapCustom fs-3">
+                  <h3>{recipe.name || "Nome non disponibile"}</h3>
+                  <p>{recipe.description || "Descrizione non disponibile"}</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        )}
+      </div>
+
+      <Link to="/RecipeSummer" 
+            state={{ fromHomepage: true }} 
+            style={{ textDecoration: "none", color: "inherit" }}
+      >
+        <h3 className="titleHomepage">Ricette estevive:</h3>
+      </Link>
+      <div className="d-flex justify-content-evenly">
+        {!recipesSummer ? (
+          <p>Caricamento delle bevande alcoliche...</p>
+        ) : recipesSummer.length === 0 ? (
+          <p>Non ci sono ricette estive disponibili al momento.</p>
+        ) : (
+          <Carousel className="CarouseCustom" interval={1500}>
+            {recipesSummer.map((recipe, index) => (
               <Carousel.Item key={index}>
                 <img
                   src={recipe.imageUrl || "immagine-default.jpg"}
